65f126c77dbf37ed56733987a3f23efe610b3b22
mjarmak <mohamadjarmak@gmail.com>
2022-10-11 22:26:31 +0200

    Added info to landing page

--- src/links/links-social.ts
+++ src/links/links-social.ts
@@ -1,9 +1,9 @@
 export const linksSocial = new Map([
+    ['linkedin', 'https://www.linkedin.com/in/mohamad-jarmak-8a0797154'],
+    ['instagram', 'https://www.instagram.com/mjarmak'],
+    ['youtube', 'https://www.youtube.com/channel/UChpZPFMIFFfcmFsjWxTF6wg'],
     ['spotify', 'https://open.spotify.com/artist/04rCfoYCCCMUteMABcJbLH'],
     ['facebook', 'https://www.facebook.com/mjarmak'],
-    ['youtube', 'https://www.youtube.com/channel/UChpZPFMIFFfcmFsjWxTF6wg'],
-    ['instagram', 'https://www.instagram.com/mjarmak'],
     ['soundcloud', 'https://soundcloud.com/mjarmak'],
-    ['linkedin', 'https://www.linkedin.com/in/mohamad-jarmak-8a0797154'],
     ['twitter', 'https://www.twitter.com/mohamadjarmak'],
 ]);
